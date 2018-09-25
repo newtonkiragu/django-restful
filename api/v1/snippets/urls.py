5330a49736b4f6666be49bf2ba87a6d0727510cc
Newton Kiragu <karanunewton4@gmail.com>
2018-09-25 06:31:31 +0300

    modified url for specific snippet

--- api/v1/snippets/urls.py
+++ api/v1/snippets/urls.py
@@ -3,5 +3,5 @@ from snippets import views
 
 urlpatterns = [
     url(r'^api/v1/snippets/$', views.snippet_list),
-    url(r'^api/v1/snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^api/v1/snippet/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
