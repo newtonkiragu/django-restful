b9a9952bcdca529ace3545d5566b665b32f13a84
Newton Kiragu <karanunewton4@gmail.com>
2018-09-25 05:56:55 +0300

    created the view to display a specific code snippet, edit or delete it

--- snippets/views.py
+++ snippets/views.py
@@ -11,6 +11,7 @@ def snippet_list(request):
     """
     List all code snippets or create a new code snippet
     """
+
     if request.method == 'GET': # fucntion to display all code snippets
         snippets = Snippet.objects.all()
         serializers = SnippetSerializer(snippets, many=True)
@@ -21,5 +22,32 @@ def snippet_list(request):
         serializer = SnippetSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return JsonResponse(serializer.data, status=201 #created)
+        return JsonResponse(serializer.errors, status=400 #bad request)
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    retrieve, update or delete a code snippet
+    """
+
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404 #not found)
+
+    if request.method == 'GET': # function to display a specific serializer
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT': # function to edit a serializer
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400 #bad request)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204 #no content)
