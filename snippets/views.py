621c41b598f6a3d622f7368e73e9a7b08f3cf857
Newton Kiragu <karanunewton4@gmail.com>
2018-09-25 05:46:16 +0300

    created a vew to display all code snippets and add code snippets

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
-# Create your views here.
+@csrf_exempt #used to prevent cross site request forgery
+def snippet_list(request):
+    """
+    List all code snippets or create a new code snippet
+    """
+    if request.method == 'GET': # fucntion to display all code snippets
+        snippets = Snippet.objects.all()
+        serializers = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializers.data, safe=False)
+
+    elif request.method == 'POST': # function to add a new snippet
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
