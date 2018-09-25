2494bee323a7d1ec26df1db51901ad9513903659
newtonkiragu <karanunewton4@gmail.com>
2018-09-25 04:35:38 +0300

    fixed syntax error in snippets model

--- snippets/models.py
+++ snippets/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from pygment.lexers import get_all_lexers
-from pygment.styles import get_all_styles
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
 
 # getting all lexers from pygment module
 LEXERS = [item for item in get_all_lexers() if item[1]]
 # getting all languages from pygment module
-LANGUAGE_CHOICES = sorted([item[1][0], item[0] for item in LEXERS])
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 class Snippet(models.Model):
