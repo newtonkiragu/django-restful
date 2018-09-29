6dfa91ade62ff9c6be82b080b5dfc4713fef55c9
newtonkiragu <karanunewton4@gmail.com>
2018-09-29 21:03:04 +0300

    created test suite

--- snippets/tests.py
+++ snippets/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+import unittest
+import json
+from django.urls import include, path, reverse
+from rest_framework.test import APITestCase, URLPatternsTestCase
+from models import Snippet
+from serializers import SnippetSerializer
 
-# Create your tests here.
+
+class SnippetTest(APITestCase, URLPatternsTestCase):
+    url_patterns = [
+        path('snippet/', include('snippets.urls'))
+    ]
+
+    def setUp(self):
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.new_snippet = {
+            "id": 1,
+            "title": "Monkey",
+            "code": "foo = \"bar\"\n",
+            "language": "python",
+            "style": "friendly"
+        }
+    
+    def tearDown(self):
+        self.app_context.pop()
