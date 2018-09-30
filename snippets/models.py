e078b39a046416ed4858e4b237f805ed8023cfbc
newtonkiragu <karanunewton4@gmail.com>
2018-09-30 20:00:13 +0300

    wrote tests

--- snippets/models.py
+++ snippets/models.py
@@ -8,6 +8,7 @@ LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
+
 class Snippet(models.Model):
     """
     A snippet model that is used to store code snippets
--- snippets/tests.py
+++ snippets/tests.py
@@ -1,9 +1,12 @@
 import unittest
 import json
 from django.urls import include, path, reverse
-from rest_framework.test import APITestCase, URLPatternsTestCase
+from rest_framework.test import APITestCase
 from . import models
 from . import serializers
+from rest_framework import status
+from rest_framework.test import APIClient
+from rest_framework import status
 
 
 class SnippetTest(APITestCase):
@@ -14,22 +17,41 @@ class SnippetTest(APITestCase):
     def setUp(self):
         self.new_snippet = models.Snippet(title='monkey', code='foo = "bar"\n', language='python', style='friendly')
 
-    def test_api_endpoints(self): pass
-
     def test_view_snippets(self):
         old_count = models.Snippet.objects.count()
         self.new_snippet.save()
         new_count = models.Snippet.objects.count()
         self.assertNotEqual(old_count, new_count)
 
-    def test_snippet_validation(self): pass
-    
-    def test_add_more_snippets(self): pass
+    def test_view_snippet(self):
+        snippet_id = 1
+        self.new_snippet.save()
+        snippet = models.Snippet.objects.get(pk=snippet_id)
+        response = self.client.get(reverse('snippet', kwargs={'pk': snippet.id}), format="json")
 
-    def test_view_snippet(self): pass
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, snippet)
 
-    def test_update_snippet(self): pass
+    def test_update_snippet(self):
+        snippet_id = 1
+        self.new_snippet.save()
+        snippet = models.Snippet.objects.get(pk=snippet_id)
+        change_snippet = {'code': 'return False'}
+        res = self.client.put(
+            reverse('snippet', kwargs={'pk': snippet.id}),
+            change_snippet, format='json'
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
 
-    def test_delete_snippet(self): pass
-    
+    def test_delete_snippet(self):
+        snippet_id = 1
+        # self.new_snippet.save()
+        snippet = models.Snippet.objects.get(pk=snippet_id)
+        response = self.client.delete(
+            reverse('snippet', kwargs={'pk': snippet.id}),
+            format='json',
+            follow=True)
+
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
+
     def tearDown(self): pass
