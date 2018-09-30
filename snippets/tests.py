c33a97c93ff9d04fed827e0fec058d8b381e55ae
newtonkiragu <karanunewton4@gmail.com>
2018-09-30 15:19:45 +0300

    wrote tests

--- snippets/tests.py
+++ snippets/tests.py
@@ -2,25 +2,34 @@ import unittest
 import json
 from django.urls import include, path, reverse
 from rest_framework.test import APITestCase, URLPatternsTestCase
-from models import Snippet
-from serializers import SnippetSerializer
+from . import models
+from . import serializers
 
 
-class SnippetTest(APITestCase, URLPatternsTestCase):
-    url_patterns = [
-        path('snippet/', include('snippets.urls'))
-    ]
+class SnippetTest(APITestCase):
+    # url_patterns = [
+    #     path('snippet/', include('snippets.urls'))
+    # ]
 
     def setUp(self):
-        self.app_context = self.app.app_context()
-        self.app_context.push()
-        self.new_snippet = {
-            "id": 1,
-            "title": "Monkey",
-            "code": "foo = \"bar\"\n",
-            "language": "python",
-            "style": "friendly"
-        }
+        self.new_snippet = models.Snippet(title='monkey', code='foo = "bar"\n', language='python', style='friendly')
+
+    def test_api_endpoints(self): pass
+
+    def test_view_snippets(self):
+        old_count = models.Snippet.objects.count()
+        self.new_snippet.save()
+        new_count = models.Snippet.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
+    def test_snippet_validation(self): pass
     
-    def tearDown(self):
-        self.app_context.pop()
+    def test_add_more_snippets(self): pass
+
+    def test_view_snippet(self): pass
+
+    def test_update_snippet(self): pass
+
+    def test_delete_snippet(self): pass
+    
+    def tearDown(self): pass
