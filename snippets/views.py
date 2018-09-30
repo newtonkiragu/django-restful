7594afe8c360cd3b20814e913d48591bb02615dd
newtonkiragu <karanunewton4@gmail.com>
2018-09-30 15:19:24 +0300

    formatted views to follow pep8

--- snippets/views.py
+++ snippets/views.py
@@ -6,24 +6,27 @@ from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
-@csrf_exempt #used to prevent cross site request forgery
+
+@csrf_exempt  # used to prevent cross site request forgery
 def snippet_list(request):
     """
     List all code snippets or create a new code snippet
     """
 
-    if request.method == 'GET': # fucntion to display all code snippets
+    if request.method == 'GET':  # fucntion to display all code snippets
         snippets = Snippet.objects.all()
         serializers = SnippetSerializer(snippets, many=True)
         return JsonResponse(serializers.data, safe=False)
 
-    elif request.method == 'POST': # function to add a new snippet
+    elif request.method == 'POST':  # function to add a new snippet
         data = JSONParser().parse(request)
+        print(data)
         serializer = SnippetSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201) #created
-        return JsonResponse(serializer.errors, status=400) #bad request
+            return JsonResponse(serializer.data, status=201)  #created
+        return JsonResponse(serializer.errors, status=400)  #bad request
+
 
 @csrf_exempt
 def snippet_detail(request, pk):
@@ -34,20 +37,20 @@ def snippet_detail(request, pk):
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
-        return HttpResponse(status=404) #not found
+        return HttpResponse(status=404)  # not found
 
-    if request.method == 'GET': # function to display a specific serializer
+    if request.method == 'GET':  # function to display a specific serializer
         serializer = SnippetSerializer(snippet)
         return JsonResponse(serializer.data)
 
-    elif request.method == 'PUT': # function to edit a serializer
+    elif request.method == 'PUT':  # function to edit a serializer
         data = JSONParser().parse(request)
         serializer = SnippetSerializer(snippet, data=data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400) #bad request
+        return JsonResponse(serializer.errors, status=400)  # bad request
 
     elif request.method == 'DELETE':
         snippet.delete()
-        return HttpResponse(status=204) #no content
+        return HttpResponse(status=204)  # no content
