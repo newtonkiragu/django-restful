638a7fc5653aed51205522088a3c100b4ee763ba
Newton Kiragu <karanunewton4@gmail.com>
2018-10-23 22:12:26 +0300

    refactored code to use response objects and wrap api views also added optional format suffixes for our urls

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippet/(?P<pk>[0-9]+)/$', views.snippet_detail, name='snippet'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
 handler404 = 'my_app.views.handler404'
 handler500 = 'my_app.views.handler500'
 handler401 = 'my_app.views.handler401'
--- snippets/views.py
+++ snippets/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+# from django.http import HttpResponse, jsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
-from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
 from rest_framework import status
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
 
 def handler404(request, *args, **argv):
@@ -35,15 +38,16 @@ def handler500(request, *args, **argv):
     return response
 
 @csrf_exempt  # used to prevent cross site request forgery
-def snippet_list(request):
+@api_view(['GET', 'POST'])
+def snippet_list(request, format=None):
     """
     List all code snippets or create a new code snippet
     """
 
     if request.method == 'GET':  # fucntion to display all code snippets
         snippets = Snippet.objects.all()
-        serializers = SnippetSerializer(snippets, many=True)
-        return JsonResponse(serializers.data, safe=False)
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
 
     elif request.method == 'POST':  # function to add a new snippet
         data = JSONParser().parse(request)
@@ -51,12 +55,13 @@ def snippet_list(request):
         serializer = SnippetSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)  # created
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # bad request
+            return Response(serializer.data, status=status.HTTP_201_CREATED)  # created
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # bad request
 
 
 @csrf_exempt
-def snippet_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk, format=None):
     """
     retrieve, update or delete a code snippet
     """
@@ -68,16 +73,16 @@ def snippet_detail(request, pk):
 
     if request.method == 'GET':  # function to display a specific serializer
         serializer = SnippetSerializer(snippet)
-        return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == 'PUT':  # function to edit a serializer
         data = JSONParser().parse(request)
         serializer = SnippetSerializer(snippet, data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # bad request
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # bad request
 
     elif request.method == 'DELETE':
         snippet.delete()
-        return HttpResponse(status.HTTP_204_NO_CONTENT)  # no content
+        return Response(status.HTTP_204_NO_CONTENT)  # no content
