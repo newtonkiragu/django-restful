3d52a955481030b8ef149db1fd259caebfb23565
Newton Kiragu <karanunewton4@gmail.com>
2018-10-23 23:21:42 +0300

    refactored code to use generic class-based views

--- snippets/views.py
+++ snippets/views.py
@@ -40,36 +40,18 @@ def handler500(request, *args, **argv):
     return response
 
 
-class SnippetList(mixins.ListModelMixin,
-    mixins.CreateModelMixin, 
-    generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     """
     List all code snippets or create a new code snippet
     """
-
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)        
-
-
-class SnippetDetails(mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView):
+class SnippetDetails(generics.RetrieveUpdateDestroyAPIView):
     """
     retrieve, update or delete a code snippet
     """
 
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
